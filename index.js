8b07892e72a03e6776292228e36de892c5a05adb
ghkim <623gahyun@gmail.com>
2021-07-08 14:21:08 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+//type으로 된 것을 분석
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ghkim:12297juny@nodebasic.sh6ll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => {console.log('MongoDB Connected...')})
 .catch(err => {console.log(err)})
 
-app.get('/', (req, res) => {res.send('Hello World!, 안녕하세요')})
+app.get('/', (req, res) => {res.send('Hello ghkim!')})
+
+app.post('/register', (req, res) => {
+    //회원가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 database에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => {console.log(`Example app listening at ${port}`)})
